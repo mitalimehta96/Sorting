54d5243dc4bb28b5f44fc79bdddc4b843a6e6dff
Mitali Mehta <37222317+mitalimehta96@users.noreply.github.com>
2018-03-19 00:22:04 +0530

    Update insertionsort.cpp

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -6,12 +6,12 @@ int main()
    int A[6];int temp,i,j,m=0;
 
     cout<<"Enter elements to be sorted with Insertion Sort\n";
-    for(i=0;i<6;i++)
+    for(i=0; i<6; i++)
       {
         cin>>A[i];
       }
     i=0;
-    for(i=i;i<6;i++)
+    for(i=i; i<6; i++)
       {
         j=i;
         while(j>0 && A[j]<A[j-1])
@@ -22,14 +22,14 @@ int main()
             j--;
           }
          cout<<"\nAfter pass "<< m+1<< "\n";
-         for(j=0;j<6;j++)
+         for(j=0; j<6; j++)
             {
               cout<< A[j]<< " ";
             }
             m++;
       }
     cout<<"\nThe sorted array is \n";
-    for(i=0;i<6;i++)
+    for(i=0; i<6; i++)
         cout<<A[i]<< " ";
     return 0;
 }
