6c873895711873186ac132c84a7a2d8410365ddf
Mitali Mehta <37222317+mitalimehta96@users.noreply.github.com>
2018-03-19 00:11:52 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -3,18 +3,21 @@ using namespace std;
 
 int main()
 {
-    int A[6];int temp,i,k,j;int n=6;
-    int flag=0;int m=0;
+    int A[6];
+    int temp,i,k,j;
+    int n=6; // Number of elements
+    int flag=0;
+    int m=0;
     cout<<"Enter elements to be sorted\n";
-    for(i=0;i<6;i++)
+    for(i=0; i<6; i++)
     {
         cin>>A[i];
     }
     temp=A[0];
-    for(k=0;k<n-1;k++)
+    for(k=0; k<n-1; k++)
     {
         flag=0;
-        for(i=0;i<n-k-1;i++)
+        for(i=0; i<n-k-1; i++)
         {
 
             if(A[i]>A[i+1])
@@ -28,11 +31,11 @@ int main()
         }
 
          cout<<"\nAfter pass "<< m+1<< "\n";
-            for(j=0;j<6;j++){
+            for(j=0; j<6; j++){
             cout<< A[j]<< " ";
             }
             m++;
-        if(flag==0)
+        if(flag == 0)
             {
         cout<<"\nIt's already sorted after phase "<<i;
         break;
@@ -40,7 +43,7 @@ int main()
 
     }
     cout<<"\nThe sorted array is \n";
-    for(i=0;i<6;i++)
+    for(i=0; i<6; i++)
         cout<<A[i]<< " ";
     return 0;
 }
